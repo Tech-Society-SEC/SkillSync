c74f2d519f37abe3444a8e80dd32a7daf72b4665
Thenmozhi N_212223060291 <166227740+Thenmozhinatraj@users.noreply.github.com>
2025-11-19 13:35:38 +0530

    Update script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -1,1 +1,27 @@
-"" 
+function sendMessage() {
+    let input = document.getElementById("userInput").value;
+    if (input.trim() === "") return;
+
+    addMessage("You: " + input, "user");
+
+    fetch("http://localhost:3000/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: input })
+    })
+    .then(res => res.json())
+    .then(data => {
+        addMessage("Bot: " + data.reply, "bot");
+    });
+
+    document.getElementById("userInput").value = "";
+}
+
+function addMessage(msg, type) {
+    let box = document.getElementById("chatbox");
+    let div = document.createElement("div");
+    div.className = type;
+    div.innerText = msg;
+    box.appendChild(div);
+    box.scrollTop = box.scrollHeight;
+}
