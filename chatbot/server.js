a8f7ea91573382c131bb93550040215dd0452794
Thenmozhi N_212223060291 <166227740+Thenmozhinatraj@users.noreply.github.com>
2025-11-19 13:36:35 +0530

    Update server.js

--- chatbot/server.js
+++ chatbot/server.js
@@ -1,1 +1,29 @@
-"" 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/chat", (req, res) => {
+    const skills = req.body.message.toLowerCase();
+
+    let reply = "I can help only if you tell your skills 🙂";
+
+    if (skills.includes("python")) {
+        reply = "You can go for Data Science, AI, Automation, Backend Development.";
+    }
+    else if (skills.includes("java")) {
+        reply = "You can explore Android Development, Spring Boot, Enterprise Software.";
+    }
+    else if (skills.includes("electronics") || skills.includes("embedded")) {
+        reply = "Best domains: Embedded Systems, IoT, Robotics, VLSI.";
+    }
+    else if (skills.includes("web") || skills.includes("html") || skills.includes("css")) {
+        reply = "Best domains: Web Development, UI/UX, Full Stack Developer.";
+    }
+
+    res.json({ reply });
+});
+
+app.listen(3000, () => console.log("Chatbot running on http://localhost:3000"));
